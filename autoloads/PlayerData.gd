4599f86397edc5358006d2b8a22e54b472ae7207
Natan Maia <natanmaiaplays@outlook.com>
2023-12-19 19:58:41 -0300

    autoload de Player Data
    usa isso pra salvar pontuação e vida durante a fase. não salvar vida no objeto do jogador.

---
+++ autoloads/PlayerData.gd
@@ -0,0 +1,33 @@
+extends Node
+
+## Emitido quando o jogador leva 3 Strikes.
+## O node do menu de derrota escuta esse signal.
+signal player_lost
+
+## Emitido quando o jogador vencer a fase.
+## Não utilizado se a fase for infinita.
+signal player_won
+
+var score : int = 0
+var strikes : int = 0
+
+## Chamar sempre antes de começar uma fase.
+func reset_game() -> void:
+	score = 0
+	strikes = 0
+
+## Score aumenta de 100 em 100, pelo psicológico de número maior ser melhor.
+func add_score() -> void:
+	score += 100
+
+## Toda vez que uma bigorna cai na cidade.
+## Se caírem 3, a partida acaba.
+func add_strike() -> void:
+	strikes += 1
+	if strikes >= 3:
+		lose_game()
+
+#TODO: Mostrar tela de derrota.
+func lose_game() -> void:
+	print_debug("VOCE PERDEU")
+	player_lost.emit()
