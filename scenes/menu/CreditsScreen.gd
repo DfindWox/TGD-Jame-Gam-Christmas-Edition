bc437ac14de39dbedc31f5fd148689b987a2c5dd
Natan Maia <natanmaiaplays@outlook.com>
2023-12-19 22:46:59 -0300

    menus

---
+++ scenes/menu/CreditsScreen.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_btn_return_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu/TitleScreen.tscn")
---
+++ scenes/menu/TitleScreen.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+
+
+
+
+func _on_btn_play_1_pressed():
+	#get_tree().change_scene_to_file("file path here")
+	pass
+
+
+func _on_btn_play_2_pressed():
+	#get_tree().change_scene_to_file("file path here")
+	pass
+
+
+func _on_btn_play_3_pressed():
+	#get_tree().change_scene_to_file("file path here")
+	pass
+
+
+func _on_btn_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu/CreditsScreen.tscn")
+	pass
